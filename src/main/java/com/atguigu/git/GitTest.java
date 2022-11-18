8824e176f95affa92439fa35c66037f75ca8229d
Zhoucq <838194050@qq.com>
2022-11-18 15:50:15 +0800

    second cimmit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 111111111111111111111111111");
+        System.out.println("hello git 222222222222222222222222222");
     }
 }
