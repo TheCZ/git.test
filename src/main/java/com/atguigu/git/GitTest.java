25dd8babbff3220e6a83b058cb0fd2bd48d20df5
Zhoucq <838194050@qq.com>
2022-11-18 16:03:42 +0800

    hot-fix cimmit-1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git 111111111111111111111111111");
         System.out.println("hello git 222222222222222222222222222");
         System.out.println("hello git 333333333333333333333333333");
+        System.out.println("hello git 444444444444444444444444444hot");
     }
 }
